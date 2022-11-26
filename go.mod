6176f020bb6249d8b2a90d619f68071f8119aea7
ganyacc <gansuyadav177@gmail.com>
2022-11-26 13:17:45 +0530

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module htmltemp
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 type Note struct {
 	Title       string
@@ -12,5 +18,21 @@ var noteStore = make(map[string]Note)
 var id int = 0
 
 func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	fs := http.FileServer(http.Dir("public"))
+	r.Handle("/public", fs)
+	r.HandleFunc("/", getNotes)
+	r.HandleFunc("/notes/add", addNote)
+	r.HandleFunc("/notes/save", saveNote)
+	r.HandleFunc("/notes/edit/{id}", editNote)
+	r.HandleFunc("/notes/update/{id}", updateNote)
+	r.HandleFunc("/notes/delete/{id}", deleteNote)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	fmt.Println("Listning at port 8080")
+	server.ListenAndServe()
 
 }
