b0c51d5b21a604f925be7c6b4e096ca14d1fbb60
ganyacc <gansuyadav177@gmail.com>
2022-11-26 15:50:33 +0530

    changes made to main.go base.html index.html

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -16,6 +17,31 @@ type Note struct {
 
 var noteStore = make(map[string]Note)
 var id int = 0
+
+var templates (map[string]*template.Template)
+
+func init() {
+	if templates == nil {
+		templates = make(map[string]*template.Template)
+
+	}
+	templates["index"] = template.Must(template.ParseFiles("template/index.html", "template/base.html"))
+	templates["add"] = template.Must(template.ParseFiles("template/add.html", "template/base.html"))
+	templates["edit"] = template.Must(template.ParseFiles("template/edit.hmtl", "template/base.html"))
+}
+
+func renderTemplate(rw http.ResponseWriter, name string, template string, viewModel interface{}) {
+	tmpl, ok := templates[name]
+	if !ok {
+		http.Error(rw, "The template does not exist.", http.StatusInternalServerError)
+		return
+	}
+
+	err := tmpl.ExecuteTemplate(rw, template, viewModel)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+	}
+}
 
 func main() {
 	r := mux.NewRouter().StrictSlash(false)
